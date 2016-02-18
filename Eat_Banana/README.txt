d617a94dad225e6453d14547de17ac95d6b936ee
mattina401 <mattina401@gmail.com>
2016-02-18 12:36:41 -0500

    first commit

---
+++ Eat_Banana/README.txt
@@ -0,0 +1,16 @@
+To start game fisrt, you should enter 'Enter' button.
+
+Control
+: UP - move up monkey
+  DOWN - move down monkey
+
+
+When you eat banana, Heart will move to right.
+And if your monkey is hit by Log, Heart will move to Left.
+
+When Heart got to end of Left side, Game Over.
+when Heart got to another monkey(pink one), you will win.
+
+
+
+Thank you.
---
+++ Eat_Banana/main.h
@@ -0,0 +1,71 @@
+void drawBanana(void);
+void drawEnemy(void);
+void colMonLog(void);
+void drawHeart(void);
+void colMonBan(void);
+void colMonGoal(void);
+void gameover(void);
+void gameover1(void);
+
+int gameon;
+
+
+typedef struct
+{
+    int x;
+    int y;
+    int height;
+    int width;
+    int oldx;
+    int oldy;
+    int isbanana;
+    int iscol;
+}BANANAS;
+
+
+typedef struct
+{
+    int x;
+    int y;
+    int height;
+    int width;
+    int oldx;
+    int oldy;
+    int isenemy;
+}ENEMYS;
+
+typedef struct
+{
+    int x;
+    int y;
+    int height;
+    int width;
+    int oldx;
+    int oldy;
+    int isplayer;
+}PLAYER;
+
+typedef struct
+{
+    int x;
+    int y;
+    int height;
+    int width;
+    int oldx;
+    int oldy;
+    int isheart;
+}HEARTS;
+
+
+typedef struct
+{
+    int x;
+    int y;
+    int height;
+    int width;
+    int oldx;
+    int oldy;
+    int isgoal;
+}GOAL;
+
+
